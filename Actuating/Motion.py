af4f51eed954db55f46a5bb9867a2271749673e8
YooSunJong <jobgsyou@naver.com>
2020-07-31 16:28:26 +0900

    Add files via upload

--- Actuating/Motion.py
+++ Actuating/Motion.py
@@ -1,27 +1,25 @@
-import platform
-import numpy as np
-import argparse
-import cv2
 import serial
 import time
-import sys
 from threading import Thread
 
-MOTION = dict()
-MOTION["WALK"] = {
+MOTION = {
+    "SIGNAL": {"INIT": 29},
+    "MODE": {"WALK": 0, "STABLE": 122, "MOVE": 15, "VIEW": 29, "TURN": 111},
 
-}
-MOTION["SPEED"] = {
+    "VIEW": {"DOWN80": 0, "DOWN60": 2, "FRONT": 5, "DOWN45": 6, "DOWN30": 7},
+    # 왼쪽, 오른쪽 각각 45도씩
+    "DIR": {"CENTER": 21, "LEFT": 22, "RIGHT": 24},
+    "SCOPE": {"SHORT": 0, "LONG": 5},
+    "SPEED": {"FAST": 0, "SLOW": 3},
 
-}
-MOTION["DIRECTION"] = {
+    "WALK": {
+        "START": 8,
+        "END": 101,
+        "FRONT": 102,
+        "BACK": 12
+    },
 
-}
-MOTION["SCOPE"] = {
-
-}
-MOTION["HEAD"] = {
-
+    "GRAB": 33
 }
 
 class Motion:
@@ -31,6 +29,7 @@ class Motion:
         self.Read_RX = 0
         self.receiving_exit = 1
         self.threading_Time = 0.01
+        #self.distance = 99
         BPS = 4800  # 4800,9600,14400, 19200,28800, 57600, 115200
 
         # ---------local Serial Port : ttyS0 --------
@@ -43,16 +42,7 @@ class Motion:
         self.serial_t.start()
         time.sleep(0.1)
 
-    def start(self): pass
-    def init(self): pass
-    def walk(self): pass
-    def head(self): pass
-    def move(self): pass
-    def turn(self): pass
-    def grab(self): pass
-
     def TX_data_py2(self, one_byte):  # one_byte= 0~255
-
         self.serial_port.write(serial.to_bytes([one_byte]))  # python3
         time.sleep(1)
 
@@ -65,7 +55,6 @@ class Motion:
             return 0
 
     def Receiving(self, ser):
-        global receiving_exit
         #
         # global X_255_point
         # global Y_255_point
@@ -73,22 +62,71 @@ class Motion:
         # global Y_Size
         # global Area, Angle
 
-        receiving_exit = 1
+        self.receiving_exit = 1
         while True:
-            if receiving_exit == 0:
+            if self.receiving_exit == 0:
                 break
             time.sleep(self.threading_Time)
+            # 수신받은 데이터의 수가 0보다 크면 데이터를 읽고 출력
             while ser.inWaiting() > 0:
+                # Rx, 수신
                 result = ser.read(1)
                 RX = ord(result)
                 print("RX=" + str(RX))
 
                 # -----  remocon 16 Code  Exit ------
                 if RX == 16:
-                    receiving_exit = 0
+                    self.receiving_exit = 0
                     break
+
+    '''def start(self):
+        self.TX_data_py2(MOTION["SIGNAL"]["START"])
+        pass'''
+
+    def init(self):
+        self.TX_data_py2(MOTION["SIGNAL"]["INIT"])
+        pass
+
+    def walk(self, speed=MOTION["SPEED"]["SLOW"], direction=MOTION["DIR"]["CENTER"]):
+        if direction != MOTION["DIR"]["CENTER"]:
+            self.TX_data_py2(direction)
+        self.TX_data_py2(MOTION["MODE"]["WALK"] + MOTION["WALK"]["START"] + speed)
+        pass
+
+    def head(self, view=MOTION["VIEW"]["DOWN80"], direction=MOTION["DIR"]["CENTER"]):
+        # print(type(view))
+        self.TX_data_py2(direction)
+        self.TX_data_py2(view)
+        return self
+
+    def move(self, direct=MOTION["DIR"]["LEFT"], repeat=1):
+        for _ in range(repeat):
+            self.TX_data_py2(MOTION["MODE"]["MOVE"] + direct)
+        pass
+
+    def turn(self, direct=MOTION["DIR"]["LEFT"], repeat=1):
+        for _ in range(repeat):
+            self.TX_data_py2(direct)
+        pass
+
+    def grab(self):
+        self.TX_data_py2(MOTION["GRAB"])
+        pass
+
+
 
 # Tx를 보낸 후 응답을 받을 때 까지 Lock을 걸기 반대쪽에서 보낸 Rx가 유실 될 수도 있으니, 일정시간이 지나도 답이 안오면 재요청
 
+
 if __name__ == '__main__':
-    pass
+    temp = Motion()
+    temp.init()
+    time.sleep(1)
+    x = 3
+    while x > 0:
+        temp.walk()
+        x -= 1
+    time.sleep(1)
+    temp.init()
+
+
