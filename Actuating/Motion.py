383eb9daba4d9abe9b7775844fe4e4ad7b06e677
YooSunJong <jobgsyou@naver.com>
2020-08-03 18:07:10 +0900

    동작+통신

--- Actuating/Motion.py
+++ Actuating/Motion.py
@@ -52,7 +52,6 @@ class Motion:
         if self.serial_port.inWaiting() > 0:
             result = self.serial_port.read(1)
             RX = ord(result)
-            self.lock = True
             return RX
         else:
             return 0
@@ -76,6 +75,7 @@ class Motion:
             # 수신받은 데이터의 수가 0보다 크면 데이터를 읽고 출력
             while ser.inWaiting() > 0:
                 # Rx, 수신
+                self.lock = True
                 result = ser.read(1)
                 RX = ord(result)
                 print("RX=" + str(RX))
@@ -91,42 +91,49 @@ class Motion:
 
     def init(self):
         self.TX_data_py2(MOTION["SIGNAL"]["INIT"])
-        while self.getRx():
-            pass
+        while not self.getRx():
+            print(self.getRx())
+        pass
 
     def walk(self, walk_signal=MOTION["WALK"]["START"], speed=MOTION["SPEED"]["SLOW"]):
         if walk_signal == MOTION["WALK"]["END"]:
             self.TX_data_py2(MOTION["MODE"]["WALK"] + walk_signal)
         self.TX_data_py2(MOTION["MODE"]["WALK"] + walk_signal + speed)
-        while self.getRx():
-            pass
+        while not self.getRx():
+            print(self.getRx())
+        pass
 
     def head(self, view=MOTION["VIEW"]["DOWN80"], direction=MOTION["DIR"]["CENTER"]):
         # print(type(view))
         self.TX_data_py2(direction)
-        while self.getRx():
-            pass
+        while not self.getRx():
+            print(self.getRx())
+        pass
+
         self.TX_data_py2(view)
-        while self.getRx():
-            pass
+        while not self.getRx():
+            print(self.getRx())
+        pass
 
     def move(self, direct=MOTION["DIR"]["LEFT"], repeat=1):
         for _ in range(repeat):
             self.TX_data_py2(MOTION["MODE"]["MOVE"] + direct)
-        while self.getRx():
-            pass
+        while not self.getRx():
+            print(self.getRx())
+        pass
 
     def turn(self, direct=MOTION["DIR"]["LEFT"], repeat=1):
         for _ in range(repeat):
             self.TX_data_py2(direct + MOTION["MODE"]["TURN"])
-        while self.getRx():
-            pass
+        while not self.getRx():
+            print(self.getRx())
+        pass
 
     def grab(self):
         self.TX_data_py2(MOTION["GRAB"])
-        while self.getRx():
-            pass
-
+        while not self.getRx():
+            print(self.getRx())
+        pass
 
 # Tx를 보낸 후 응답을 받을 때 까지 Lock을 걸기 반대쪽에서 보낸 Rx가 유실 될 수도 있으니, 일정시간이 지나도 답이 안오면 재요청
 
