e2e09365469d01b4381ab665ba76ec3fe3b644a9
parkjeonghwan <jpark0902@kookmin.ac.kr>
2020-08-10 22:09:28 +0900

    test_obstacle avoidance

--- Sensing/ImageProcessing.py
+++ Sensing/ImageProcessing.py
@@ -62,7 +62,7 @@ class ImageProcessor:
     def getBinImage(self, color="RED", debug=False): # 인자로 넘겨 받은 색상만 남기고 리턴
         img = self.getImage()
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower1, lower2, lower3  = COLORS[color]["lower"]
+        lower1, lower2, lower3 = COLORS[color]["lower"]
         upper1, upper2, upper3 = COLORS[color]["upper"]
         img_mask1 = cv2.inRange(img_hsv, np.array(lower1), np.array(upper1))
         img_mask2 = cv2.inRange(img_hsv, np.array(lower2), np.array(upper2))
@@ -123,11 +123,7 @@ class ImageProcessor:
     def selectObject_mean(self):
         centers = []
         img_color, img_mask = self.getBinImage("RED")
-        img_show = self.getImage()
-        img_show = self.getImage()
-        img_show = self.getImage()
-
-        self.debug(img_show)
+        self.debug(img_color)
         # 등고선 따기
         contours, hierarchy = cv2.findContours(img_mask, cv2.RETR_TREE,
                                                cv2.CHAIN_APPROX_SIMPLE)
