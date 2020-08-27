1e660c47ef18c99183e6ea001278770019a1b0d6
jeonghwanPark <jpark0902@kookmin.ac.kr>
2020-08-28 04:12:25 +0900

    tracking 코드만 분리

--- Sensing/CameraSensor.py
+++ Sensing/CameraSensor.py
@@ -3,7 +3,8 @@ import time
 # import argparse
 # import multiprocessing
 # from .ImageProcessing import ImageProcessor
-CAM_ID = 0 
+CAM_ID = 0
+
 class Camera:
     def __init__(self, fps=0.1):
         self.fps = fps
@@ -15,14 +16,11 @@ class Camera:
         self.cap.set(4, self.height)
         self.fourcc = None
         self.out = None
-
-        
-
         
     def produce(self, consumer, record=True, filename="out"):
         if record:
-            self.fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # 녹화기 생성
-            self.out = cv2.VideoWriter(str(filename)+'.avi', self.fourcc, 30.0, (self.width, self.height))
+            self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 녹화기 생성
+            self.out = cv2.VideoWriter(str(filename)+'.mp4', self.fourcc, 30.0, (self.width*2, self.height*2))
 
         while (True):
             ret, frame = self.cap.read()
@@ -31,7 +29,7 @@ class Camera:
                 continue
             consumer.updateImage(frame)
             if record:
-                self.out.write(frame)
+                self.out.write(consumer.debug())
             time.sleep(self.fps)
             # 업데이트 속도 초당 몇 프레임
 
