1ddbc0d94ff971f50e567e23fa7bdc447fa50ee7
jeonghwanPark <jpark0902@kookmin.ac.kr>
2020-09-01 19:28:38 +0900

    영상녹화 다시 수정

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Humanoid Robot Sports 2020. Online
-website : http://www.seoultechrobot.com/
-## 1) Member 
-조혜영, 유선종, 박정환
+- **대회 공식 홈페이지** : http://www.seoultechrobot.com/
 
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/TQZb5LhY7eA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+**1. 팀원 소개**
+----
+멤버 | 연락처 | 프로젝트 역할
+------------ | ------------- | -------------
+유선종| [![Github Badge](https://img.shields.io/badge/YooSunJong-181717?style=flat-square&logo=github&logoColor=white&link=https://github.com/YooSunJong)](https://github.com/YooSunJong)| 모션제어
+조혜영 | [![Gmail Badge](https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=mailto:jpark0902@kookmin.ac.kr)](mailto:jpark0902@kookmin.ac.kr) [![Github Badge](https://img.shields.io/badge/dodo486-181717?style=flat-square&logo=github&logoColor=white&link=https://github.com/dodo486)](https://github.com/dodo486)| 영상처리
+박정 | [![Gmail Badge](https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=mailto:jpark0902@kookmin.ac.kr)](mailto:jpark0902@kookmin.ac.kr) [![Github Badge](https://img.shields.io/badge/jeonghi-181717?style=flat-square&logo=github&logoColor=white&link=https://github.com/jeonghi)](https://github.com/jeonghi)| 영상처리
+**2. 시현 및 알고리즘 인식률**
+---- 
+[![Video Label](http://img.youtube.com/vi/TQZb5LhY7eA/0.jpg)](https://www.youtube.com/embed/TQZb5LhY7eA")
--- test/test1.py
+++ test/test1.py
@@ -1,30 +1,51 @@
+from threading import Thread, Lock
 import cv2
-import imutils
-import numpy as np
-filename1 = "./image/IMG_1582.jpeg"
-filename2 = "./image/test.jpg"
-src = cv2.imread(filename2,1)
-# gray scale 로 변환
-gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-# canny edge detection
-edges = imutils.auto_canny(src)
-# 아래에서 위로 채우기
-h, w = edges.shape[:2]
-max_row_inds = h - np.argmax(edges[::-1], axis=0)
-row_inds = np.indices((h, w))[0]
-inds_after_edges = row_inds >= max_row_inds
-filled_from_bottom = np.zeros((h, w),dtype=np.uint8)
-filled_from_bottom[inds_after_edges] = 255
-# 침식, 팽창 연산
-vertical_size = int(w/30)
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (vertical_size,vertical_size))
-erosion = cv2.erode(filled_from_bottom,kernel,iterations = 1)
-dilate = cv2.dilate(erosion, kernel, iterations=1)
-dilate = cv2.GaussianBlur(dilate,(5,5),0)
-# 비트연산
-dst = cv2.bitwise_and(gray, gray, mask=dilate)
-cv2.imshow("dst", dst)
 
+class WebcamVideoStream :
+    def __init__(self, src = 0, width = 320, height = 240) :
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+        self.stream.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.started = False
+        self.read_lock = Lock()
 
-cv2.waitKey(0)
-cv2.destroyWindow()
+    def start(self) :
+        if self.started :
+            print "already started!!"
+            return None
+        self.started = True
+        self.thread = Thread(target=self.update, args=())
+        self.thread.start()
+        return self
+
+    def update(self) :
+        while self.started :
+            (grabbed, frame) = self.stream.read()
+            self.read_lock.acquire()
+            self.grabbed, self.frame = grabbed, frame
+            self.read_lock.release()
+
+    def read(self) :
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
+
+    def stop(self) :
+        self.started = False
+        self.thread.join()
+
+    def __exit__(self, exc_type, exc_value, traceback) :
+        self.stream.release()
+
+if __name__ == "__main__" :
+    vs = WebcamVideoStream().start()
+    while True :
+        frame = vs.read()
+        cv2.imshow('webcam', frame)
+        if cv2.waitKey(1) == 27 :
+            break
+
+    vs.stop()
+    cv2.destroyAllWindows()
